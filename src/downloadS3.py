b901779703ef50187dcec1b7b2c6717d7921dfb6
Tomas Allami <83366083+Tallami@users.noreply.github.com>
2023-03-23 11:02:55 -0300

    Update downloadS3.py

--- src/downloadS3.py
+++ src/downloadS3.py
@@ -1,35 +1,31 @@
 import boto3
 import os
 
-
 def download_s3(bucket_name, file_key, download_path):
     """
-    Descarga archivos de un bucket de Amazon S3 y los guarda en una ruta
-
-    1 - bucket_name = Nombre del bucket de S3
-    2 - file_key = Nombre del archivo en el bucket
-    3 - download_path = Ruta de guardado del archivo descargado
-
+    Download files from an Amazon S3 bucket and save them to a specified path.
+    1 - bucket_name: Name of the S3 bucket
+    2 - file_key: Name of the file in the bucket
+    3 - download_path: Path where the downloaded file will be saved
     """
-    # Recurso de S3
+    # S3 Resource
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
 
-    # Ruta de descarga
+    # Download Path
     file_name = os.path.basename(file_key)
     download_file_path = os.path.join(download_path, file_name)
 
-    # Descarga
+    # Download
     bucket.download_file(file_key, download_file_path)
 
-
-# Disney CSV
+# Download Disney CSV from S3 Bucket
 bucket_name = 'tallami-bucket'
 file_key = 'disney_plus_titles.csv'
 download_path = './data/raw'
 download_s3(bucket_name, file_key, download_path)
 
-# Netflix CSV
+# Download Netflix CSV from S3 Bucket
 bucket_name = 'tallami-bucket'
 file_key = 'netflix_titles.csv'
 download_path = './data/raw'
